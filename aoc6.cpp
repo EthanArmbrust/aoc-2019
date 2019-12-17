580fcb32c647ac4653d1ecc007a674a5edc2d648
Ethan Armbrust <EthanArmbrust@github.com>
2019-12-17 11:37:48 -0500

    working on day 6 again

--- aoc6.cpp
+++ aoc6.cpp
@@ -14,6 +14,27 @@ bool operator<(const Planet& lhs, const Planet& rhs){
 	return lhs.name < rhs.name;
 }
 
+string findRoot(vector<Planet> &planets){
+	for(auto p : planets){
+		bool foundParent = false;
+		for(int i = 0; i < planets.size(); i++){
+			for(auto c : planets[i].children){
+				if(p.name == c){
+					foundParent = true;
+					break;
+				}
+			}
+			if(foundParent){
+				break;
+			}
+		}
+		if(!foundParent){
+			return p.name;
+		}
+	}
+	return "NO_ROOT_FOUND";
+}
+
 int main(){
 
 	ifstream infile("input/6.txt");
@@ -21,18 +42,21 @@ int main(){
 	vector<Planet> planets;
 	while(getline(infile,s)){
 		Planet p = {s.substr(0, 3), {s.substr(4,3)}};
-		auto it = lower_bound(planets.begin(), planets.end(), p);
-		if(*it->name == p.name){
-			*it->children.push_back(p.children[0]);
+		if(planets.size() > 0){
+			auto it = lower_bound(planets.begin(), planets.end(), p);
+			int idx = it - planets.begin();
+			if(planets[idx].name == p.name){
+				planets[idx].children.push_back(p.children[0]);
+			}
+			else{
+				planets.insert(it, p);
+			}
 		}
 		else{
-			planets.insert(it, p);
+			planets.push_back(p);
 		}
 	}
-
-	for(auto p : planets){
-		cout << p.name << endl;
-	}
+	cout << findRoot(planets) << endl;
 	
 	return 0;
 }
