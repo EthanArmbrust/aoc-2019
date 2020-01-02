b3c2f66f9c9584ff6509d8288165ca864e6a6029
EthanArmbrust <ethanarm@buffalo.edu>
2020-01-02 00:24:44 -0500

    day 8 done, day 10 part 1 done

---
+++ aoc10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int compute_asteroids(vector<string> &a, int x, int y){
+    set<double> angles;
+    for(int j = 0; j < a.size(); j++){
+        for(int i = 0; i < a[j].length(); i++){
+            if(a[j][i] == '#'){
+                angles.insert(atan2((double)(j - y), (double)(i - x)));
+            }
+        }
+    }
+    return angles.size();
+}
+
+int main(){
+
+    ifstream infile("input/10.txt");
+    string s;
+    vector<string> input;
+    while(getline(infile,s)){
+        input.push_back(s);
+    }
+
+    int max_ass = 0;
+    for(int j = 0; j < input.size(); j++){
+        for(int i = 0; i < input[j].length(); i++){
+            if(input[j][i] == '#'){
+                max_ass = max(max_ass, compute_asteroids(input, i, j));
+            }
+        }
+    }
+
+    cout << max_ass << endl;
+
+    return 0;
+}
