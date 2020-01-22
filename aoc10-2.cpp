a951eda8ccc3a3869f609a1b97fdde7f1ee358a6
Ethan Armbrust <EthanArmbrust@github.com>
2020-01-22 15:30:45 -0500

    day 10 part 2 done

--- aoc10-2.cpp
+++ aoc10-2.cpp
@@ -13,8 +13,16 @@ struct Asteroid{
     int y;
 };
 
+#define CENTER_X 27
+#define CENTER_Y 19
+
 bool compareAst(const Asteroid &a, const Asteroid &b){
-    return a.angle > b.angle;
+	if(a.angle == b.angle){
+		return (abs(a.x - CENTER_X) + abs(a.y - CENTER_Y)) < (abs(b.x - CENTER_X) + abs(b.y - CENTER_Y));
+	}
+	else{
+		return a.angle < b.angle;
+	}
 }
 
 int compute_asteroids(vector<string> &a, int x, int y){
@@ -33,9 +41,9 @@ vector<Asteroid> list_asteroids(vector<string> &a, int x, int y){
     vector<Asteroid> as;
     for(int j = 0; j < a.size(); j++){
         for(int i = 0; i < a[j].length(); i++){
-            if(a[j][i] == '#'){
-		Asteroid a = {atan2((double)(j - y), (double)(i - x)), i, j};
-		as.push_back(a);
+            if(a[j][i] == '#' && !(j == y && i == x)){
+				Asteroid a = {atan2((double)(j - y), (double)(i - x)), i, j};
+				as.push_back(a);
             }
         }
     }
@@ -51,8 +59,8 @@ int main(){
         input.push_back(s);
     }
 
-    int center_x = 27;
-    int center_y = 19;
+    int center_x = CENTER_X;
+    int center_y = CENTER_Y;
     int as_num = 200;
 
     vector<Asteroid> as = list_asteroids(input, center_x, center_y);
@@ -62,19 +70,21 @@ int main(){
     int pos = 0;
 
     int destroy_x, destroy_y;
+	double start = atan2(-1.0, 0.0);
 
-    while(as[pos].angle < atan2(1.0, 0.0)){
-	pos++;
+    while(as[pos].angle < start){
+		pos++;
     }
 
     while(destroyed < as_num){
-        Asteroid current = as[pos];
-	destroy_x = current.x;
-	destroy_y = current.y;
-	as.erase(as.begin() + pos);
-	while(current.angle == as[pos].angle){
-	   pos = (pos + 1) % as.size();
-	}
+		Asteroid current = as[pos];
+		destroy_x = current.x;
+		destroy_y = current.y;
+		as.erase(as.begin() + pos);
+		destroyed++;
+		while(current.angle == as[pos].angle){
+		   pos = (pos + 1) % as.size();
+		}
     }
 
     cout << destroy_x << "," << destroy_y << endl;
