00cbbc0df6901bd99050c89c45167b1fc469c000
EthanArmbrust <ethanarm@buffalo.edu>
2020-02-18 16:53:40 -0500

    started day 12

---
+++ aoc12.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void update_vel(vector<vector<int>> &moon_pos, vector<vector<int>> &moon_vel){
+
+}
+
+int main(){
+
+    vector < vector < int > > moon_pos = {
+        {17, -7, -11},
+        {1, 4, -1},
+        {6, -2, -6},
+        {19, 11, 9}
+    };
+    vector < vector < int > > moon_vel = {
+        {0,0,0},
+        {0,0,0},
+        {0,0,0},
+        {0,0,0}
+    };
+
+	return 0;
+}
+
+
